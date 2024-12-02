200d2334947d108e5c158fa2627015ee88a29570
titohajili <maqatoto963@gmail.com>
2024-12-02 23:55:16 +0400

    TransactionList Display

--- tito/src/components/TransactionList.vue
+++ tito/src/components/TransactionList.vue
@@ -2,15 +2,41 @@
 
 <h3>History</h3>
       <ul id="list" class="list">
-        <li class="minus">
-          Cash <span>-$400</span><button class="delete-btn">x</button>
+        <ul class="list" id="list">
+          {{ transaction.text }}
+          <li class="minus">
+          Cash <span>{{transaction.amount}}</span>
+          
+          <button class="delete-btn">x</button>
         </li>
+
+      </ul>
+
+        <!-- 
 
         <li class="plus">
           Paycheck <span>$800</span><button class="delete-btn">x</button>
-        </li>
+        </li> -->
 
       </ul>
 
 </template>
 
+<script>
+
+
+
+export default {
+  data(){
+     return {
+       transactions: [
+        {id: 1, text: 'Flower', amount: -19.99},
+        {id: 2, text: 'Salary', amount: 3000}, 
+        {id: 3, text: 'Book', amount: -10},
+        {id: 4, text: 'Camera', amount: 150}
+       ]
+     }
+  }
+}
+
+</script>
