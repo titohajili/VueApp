d2e898c40b93dd738ba267ca5675093d3e44e430
titohajili <maqatoto963@gmail.com>
2024-12-04 23:11:30 +0400

    transaction list

--- tito/src/App.vue
+++ tito/src/App.vue
@@ -13,26 +13,22 @@
 
 </template>
 
-<script>
+<script setup>
 import Balance from '@/components/Balance.vue';
 import Headers from '@/components/Headers.vue';
 import IncomeExpenses from '@/components/IncomeExpenses.vue';
 import TransactionList from '@/components/TransactionList.vue'
 import AddTransaction from '@/components/AddTransaction.vue'
 
+import { ref } from 'vue';
 
-export default {
+const transactions = ref([
+    { id: 1, text: 'Paycheck', amount: 800 },
+    { id: 2, text: 'Car Payment', amount: -200 },
+    { id: 3, text: 'Groceries', amount: -50 },
+    { id: 4, text: 'Gas', amount: -100 },
+  ]);
 
-     name: "AppHeader",
-    
-    components: {
-        Headers,
-        Balance,
-        IncomeExpenses,
-        TransactionList,
-        AddTransaction
-    },
-}
 
 </script>
 
--- tito/src/components/TransactionList.vue
+++ tito/src/components/TransactionList.vue
@@ -2,16 +2,11 @@
 
 <h3>History</h3>
       <ul id="list" class="list">
-        <ul class="list" id="list">
-          {{ transaction.text }}
-          <li class="minus">
-          Cash <span>{{transaction.amount}}</span>
-          
-          <button class="delete-btn">x</button>
+        <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }} <span>${{ transaction.amount }}</span> <button class="delete-btn">x</button>
         </li>
 
-      </ul>
-
+      
         <!-- 
 
         <li class="plus">
@@ -22,21 +17,3 @@
 
 </template>
 
-<script>
-
-
-
-export default {
-  data(){
-     return {
-       transactions: [
-        {id: 1, text: 'Flower', amount: -19.99},
-        {id: 2, text: 'Salary', amount: 3000}, 
-        {id: 3, text: 'Book', amount: -10},
-        {id: 4, text: 'Camera', amount: 150}
-       ]
-     }
-  }
-}
-
-</script>
